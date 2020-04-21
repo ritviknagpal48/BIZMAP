907dc51c17915fed3558cb090c7c18d71598ca96
Nikhil-codecracker <nikhilgupta1489@gmail.com>
2020-04-21 19:42:41 +0530

    removed logo

--- client/src/App.js
+++ client/src/App.js
@@ -32,49 +32,7 @@ function App() {
               >
                 <i className='ti-menu ti-close' />
               </a>
-              {/* ============================================================== */}
-              {/* Logo */}
-              {/* ============================================================== */}
-              <a className='navbar-brand' href='#'>
-                {/* Logo icon */}
-                <b className='logo-icon'>
-                  {/*You can put here icon as well // <i class="wi wi-sunset"></i> //*/}
-                  {/* Dark Logo icon */}
-                  <img
-                    src='./Xtreme admin Template - The Ultimate Multipurpose admin template_files/logo-icon.png'
-                    alt='homepage'
-                    className='dark-logo'
-                  />
-                  {/* Light Logo icon */}
-                  <img
-                    src='./Xtreme admin Template - The Ultimate Multipurpose admin template_files/logo-light-icon.png'
-                    alt='homepage'
-                    className='light-logo'
-                  />
-                </b>
-                {/*End Logo icon */}
-                {/* Logo text */}
-                <span className='logo-text'>
-                  {/* dark Logo text */}
-                  <img
-                    src='./Xtreme admin Template - The Ultimate Multipurpose admin template_files/logo-text.png'
-                    alt='homepage'
-                    className='dark-logo'
-                  />
-                  {/* Light Logo text */}
-                  <img
-                    src='./Xtreme admin Template - The Ultimate Multipurpose admin template_files/logo-light-text.png'
-                    className='light-logo'
-                    alt='homepage'
-                  />
-                </span>
-              </a>
-              {/* ============================================================== */}
-              {/* End Logo */}
-              {/* ============================================================== */}
-              {/* ============================================================== */}
-              {/* Toggle which is visible on mobile only */}
-              {/* ============================================================== */}
+
               <a
                 className='topbartoggler d-block d-md-none waves-effect waves-light'
                 href='javascript:void(0)'
